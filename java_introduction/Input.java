1306f0cb4355bdeca8f75d10dff5b63043b05965
vandhana <vandhanavishnuraj27@gmail.com>
2026-02-06 23:43:53 +0530

    Input.java file added

---
+++ java_introduction/Input.java
@@ -0,0 +1,8 @@
+package java_introduction;
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine()); // takes line of input from user (string)
+    }
+}
--- java_introduction/Main.java
+++ java_introduction/Main.java
@@ -1,12 +1,16 @@
+package java_introduction;
 // Every Java file is a class in itself
 // The name of the file and the public class name should be the same
 public class Main { // public class is accessible from other classes, files, packages, etc.
     public static void main(String[] args){
         // System.out.println("Hello World");
-        System.out.println(args[0]); 
+        System.out.println(args[0]); // prints first command line argument - the command line arguments are stored as an array ('args variable').
 }}
 
 // public - since main function is the entry point of the program, the main function is made publicly accessible
 // static - we are having static here because we want to execute the main function without creating an object of the class Main
 // void - we do not want the main function to return any anything
 // String[] args - command line arguments of array of strings type - e.g: java Main 30 -> Output: 30 (iF System.out.orintln(args[0]))
+
+// out is standard output stream. The value of out is null, which is the command line - it prints the command line output by default. If you want to print output from file, then out = File.
+
